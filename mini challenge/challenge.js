0a6bbe6e19a7b7a95fbfbf509d405c001b285342
Mason Shirhall <mason.shirhall@vtsd.com>
2022-01-18 11:07:30 -0500

    start of mini challenge

---
+++ mini challenge/challenge.js
@@ -0,0 +1,9 @@
+/*
+Design a page that when you change a text input, an image gets bigger
+*/
+function codes(){
+  var = document.getElementById("text")
+
+  var = document.getElementById("img")
+
+}
